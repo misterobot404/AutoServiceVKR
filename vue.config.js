4000c8cf99db30650774e910a8fe93d992223cb8
misterobot404knastu <misterobot404@gmail.com>
2021-06-23 13:21:20 +1000

    first

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
+
+module.exports = {
+    outputDir: 'docs',
+    configureWebpack: {
+        plugins: [
+            new VuetifyLoaderPlugin()
+        ]
+    },
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/AutoServiceVKR/'
+        : '/'
+}
